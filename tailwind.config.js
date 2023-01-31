8faa42f29bab753c78e50a51157bd31fdfe6981e
Andreas Remdt <me@andreasremdt.com>
2023-01-31 11:59:30 +0100

    chore: install and configure eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}',
-  ],
-  darkMode: 'class',
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
