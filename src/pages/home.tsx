96f6e113d16436573b9cfbc155aec07f4ca4de1f
Andreas Remdt <me@andreasremdt.com>
2023-02-09 16:41:09 +0100

    feat: display user recipes on home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,28 @@
-import type { Component } from "solid-js";
+import { Component, createResource, Index } from "solid-js";
 
 import MainLayout from "../layouts/main";
+import fetcher from "../lib/fetcher";
+import { Recipe } from "../types/recipe";
 
-const Home: Component = () => {
+const fetchRecipes = async () => fetcher<Recipe[]>("GET", `/api/recipes`);
+
+const Page: Component = () => {
+  const [recipes] = createResource(fetchRecipes);
+
   return (
     <MainLayout title="My Recipes">
-      <p>Hello World</p>
+      <ul class="list-disc pl-4">
+        <Index each={recipes()}>
+          {(recipe) => (
+            <li>
+              <div>{recipe().title}</div>
+              <div>{recipe().description}</div>
+            </li>
+          )}
+        </Index>
+      </ul>
     </MainLayout>
   );
 };
 
-export default Home;
+export default Page;
